54fb1800216af1bc1ba0369181bceca4da92c726
ThiccPan <ihilmiderian@gmail.com>
2024-10-23 17:48:25 +0700

    feat: init firebase config and integration

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,39 @@
 // src/index.ts
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
+import GetNotes from "./handler/NotesHandler";
+
+import 'dotenv/config'
+import { error } from "console";
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 
 dotenv.config();
+const firebaseSecret = process.env.FIREBASE_SECRET
+if (!firebaseSecret) {
+  error("firebase secret env var not available")
+  process.exit(1)
+}
+
+const initFirebase = () => {
+  const credentials = JSON.parse(firebaseSecret); 
+  const firebaseApp = initializeApp({
+    credential: cert(credentials)
+  });
+
+  return firebaseApp
+}
+
+const firebaseService = initFirebase()
+export const db = getFirestore(firebaseService)
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+const router = express.Router();
+router.get("/notes", GetNotes);
 
+app.use("/v1", router);
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
