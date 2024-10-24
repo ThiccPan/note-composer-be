bce40667d206c5aa9270d7321af490d6ebf3c3ac
ThiccPan <ihilmiderian@gmail.com>
2024-10-24 10:23:05 +0700

    refactor: delete unused console log calls

--- src/middleware/ValidationMiddleware.ts
+++ src/middleware/ValidationMiddleware.ts
@@ -3,15 +3,14 @@ import { z, ZodError } from 'zod';
 
 import { StatusCodes } from 'http-status-codes';
 
+
 export function validateData(schema: z.ZodObject<any, any>) {
-  return (req: Request, res: Response, next: NextFunction) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      console.log("req body", req.body)
       schema.parse(req.body);
       next();
     } catch (error) {
       if (error instanceof ZodError) {
-        console.log(error.errors)
         const errorMessages = error.errors.map((issue: any) => ({
           message: `${issue.path.join('.')} is ${issue.message}`,
         }))
