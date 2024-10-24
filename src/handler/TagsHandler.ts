6d8c623c96ab420b2cc77e7a10189f642df1f470
ThiccPan <ihilmiderian@gmail.com>
2024-10-24 10:25:02 +0700

    feat: handler and route for adding, getting, and deleting tags

---
+++ src/handler/TagsHandler.ts
@@ -0,0 +1,118 @@
+import { Request, Response } from "express";
+import { AuthCredentials, Note, Tag } from "../types/types";
+import { db } from "..";
+import { StatusCodes } from "http-status-codes";
+
+const GetTags = async (req: Request, res: Response) => {
+  const userData: AuthCredentials = res.locals.userData
+  const isNoteCollectionExist = await checkIfUserNotesExist(userData.id)
+  if (!isNoteCollectionExist) {
+    res.status(StatusCodes.BAD_REQUEST)
+    throw Error('users not created yet')
+  }
+
+  const data = await db.collection(`/notes/${userData.id}/tags`).get()
+  if (data.empty) {
+    res.status(StatusCodes.NOT_FOUND).json({ error: "no tags found" })
+    return
+  }
+  let tagsList: Tag[] = []
+  data.forEach(doc => {
+    const tagData: Tag = {
+      id: doc.id,
+      description: doc.data().description
+    }
+    tagsList.push(tagData)
+  })
+
+  res.json({
+    "message": "success",
+    "data": tagsList
+  })
+}
+
+const AddTags = async (req: Request, res: Response) => {
+  try {
+    const userData: AuthCredentials = res.locals.userData
+    const isNoteCollectionExist = await checkIfUserNotesExist(userData.id)
+    if (!isNoteCollectionExist) {
+      res.status(StatusCodes.BAD_REQUEST)
+      throw Error('users not created yet')
+    }
+
+    let reqNewTag = req.body as Tag
+    console.log("body:", reqNewTag)
+    const docRef = db.collection(`/notes/${userData.id}/tags`).doc(reqNewTag.id)
+    const isTagExist = (await docRef.get()).exists
+    if (isTagExist) {
+      res.status(400).json({ message: "tag already exist" })
+      return
+    }
+    await docRef.set({
+      description: reqNewTag.description ? reqNewTag.description : "",
+      notes: []
+    });
+    res.json({ message: "success adding new tag" });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+}
+
+const UpdateTags = async (req: Request, res: Response) => {
+
+}
+
+const DeleteTags = async (req: Request, res: Response) => {
+  try {
+    const userData = res.locals.userData as AuthCredentials
+    const isNoteCollectionExist = await checkIfUserNotesExist(userData.id)
+    if (!isNoteCollectionExist) {
+      res.status(StatusCodes.BAD_REQUEST)
+      throw Error('users not created yet')
+    }
+
+    const tagId = req.params.id
+    const tagDocRef = db.collection(`/notes/${userData.id}/tags`).doc(tagId)
+    await db.runTransaction(async (t) => {
+      const tagFetchRes = await t.get(tagDocRef)
+      if (!tagFetchRes.exists) {
+        res.status(404)
+        throw Error("tag is not found")
+      }
+      console.log("deleting tag notes")
+
+      // delete tag that occur in each notes where the tag is used
+      const tagNotesDocRef = tagDocRef.collection("tagNotes")
+      const tagNotesDocList = await tagNotesDocRef.listDocuments()
+      tagNotesDocList.forEach((tagNoteDoc) => {
+        const noteIdToUpdate = tagNoteDoc.id
+        const noteDocRefToUpdate = db.collection(`notes/${userData.id}/notes`).doc(noteIdToUpdate)
+
+        const noteTagDocRefToDelete = noteDocRefToUpdate.collection("noteTags").doc(tagId)
+        t.delete(noteTagDocRefToDelete)
+      })
+
+      // delete tag data
+      t.delete(tagDocRef)
+    })
+    res.json({ message: "tag successfully deleted" });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+}
+
+const checkIfUserNotesExist = async (userId: string): Promise<boolean> => {
+  const tagDocRef = db.collection(`/notes/`).doc(userId)
+  const tagDocData = await tagDocRef.get()
+  return tagDocData.exists
+}
+
+export { GetTags, AddTags, UpdateTags, DeleteTags }
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import GetNotes from "./handler/NotesHandler";
+import { AddTags, DeleteTags, GetTags } from "./handler/TagsHandler";
 
 import 'dotenv/config'
 import { error } from "console";
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
+import { getAuth } from 'firebase-admin/auth';
+import { validateData } from "./middleware/ValidationMiddleware";
+import { addTagsSchema } from "./schemas/TagsSchema";
+import bodyParser from "body-parser";
+import { AuthMiddleware, MockAuthMiddleware } from "./middleware/AuthMiddleware";
 
 dotenv.config();
 const firebaseSecret = process.env.FIREBASE_SECRET
@@ -16,7 +22,7 @@ if (!firebaseSecret) {
 }
 
 const initFirebase = () => {
-  const credentials = JSON.parse(firebaseSecret); 
+  const credentials = JSON.parse(firebaseSecret);
   const firebaseApp = initializeApp({
     credential: cert(credentials)
   });
@@ -26,14 +32,22 @@ const initFirebase = () => {
 
 const firebaseService = initFirebase()
 export const db = getFirestore(firebaseService)
+export const auth = getAuth(firebaseService)
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
+app.use(bodyParser.json());
 
-const router = express.Router();
-router.get("/notes", GetNotes);
+const tagsRouter = express.Router();
+tagsRouter.use(MockAuthMiddleware())
+tagsRouter.get("/tags", GetTags);
+tagsRouter.post("/tags", validateData(addTagsSchema), AddTags);
+tagsRouter.delete("/tags/:id", DeleteTags);
 
-app.use("/v1", router);
+const notesRouter = express.Router();
+notesRouter.get("/notes", GetNotes);
+
+app.use("/v1", tagsRouter, notesRouter);
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
