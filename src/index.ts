9282997e61854aba794ed1fc06496fc1cf92aa28
ThiccPan <ihilmiderian@gmail.com>
2024-11-01 22:11:41 +0700

    feat: add route and middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // src/index.ts
 import express, { Express } from "express";
 import dotenv from "dotenv";
-import { AddNotes, DeleteNotes, GetNotes } from "./handler/NotesHandler";
+import { AddNotes, DeleteNotes, GetNoteById, GetNotes, UpdateNotes } from "./handler/NotesHandler";
 import { AddTags, DeleteTags, GetTags } from "./handler/TagsHandler";
 
 import 'dotenv/config'
@@ -16,6 +16,7 @@ import { AuthMiddleware, MockAuthMiddleware } from "./middleware/AuthMiddleware"
 import { registerUserSchema } from "./schemas/UsersSchema";
 import { GetCurrentUserData, RegisterUsers } from "./handler/UserHandler";
 import { addNoteSchema } from "./schemas/NotesSchema";
+import cors from "cors"
 
 dotenv.config();
 const firebaseSecret = process.env.FIREBASE_SECRET
@@ -38,24 +39,26 @@ export const db = getFirestore(firebaseService)
 export const auth = getAuth(firebaseService)
 
 const app: Express = express();
+app.use(cors())
 const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 
 const usersRouter = express.Router();
-usersRouter.use(MockAuthMiddleware())
-usersRouter.get('/users', GetCurrentUserData)
-usersRouter.post('/users/register', validateData(registerUserSchema), RegisterUsers)
+usersRouter.get('/users', AuthMiddleware(), GetCurrentUserData)
+usersRouter.post('/users/register', AuthMiddleware(), validateData(registerUserSchema), RegisterUsers)
 
 const tagsRouter = express.Router();
-tagsRouter.use(MockAuthMiddleware())
+tagsRouter.use(AuthMiddleware())
 tagsRouter.get("/tags", GetTags);
 tagsRouter.post("/tags", validateData(addTagsSchema), AddTags);
 tagsRouter.delete("/tags/:id", DeleteTags);
 
 const notesRouter = express.Router();
-tagsRouter.use(MockAuthMiddleware())
+tagsRouter.use(AuthMiddleware())
 notesRouter.get("/notes", GetNotes);
 notesRouter.post("/notes", validateData(addNoteSchema), AddNotes);
+notesRouter.get("/notes/:id", GetNoteById);
+notesRouter.put("/notes/:id", UpdateNotes);
 notesRouter.delete("/notes/:id", DeleteNotes);
 
 app.use("/v1", usersRouter, tagsRouter, notesRouter);
