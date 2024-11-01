fc896884b706742188b539cfbce05cde96dd1e70
ThiccPan <ihilmiderian@gmail.com>
2024-11-01 22:07:07 +0700

    feat: add types and schema for note and tags

--- src/schemas/NotesSchema.ts
+++ src/schemas/NotesSchema.ts
@@ -5,3 +5,15 @@ export const addNoteSchema = z.object({
   content: z.string(),
   tags: z.array(z.string())
 }).partial({ content: true })
+
+export type addNoteType = z.infer<typeof addNoteSchema>
+
+export const updateNoteSchema = z.object({
+  title: z.string(),
+  content: z.string(),
+}).partial({
+  title: true,
+  content: true,
+})
+
+export type updateNoteType = z.infer<typeof updateNoteSchema>
--- src/schemas/TagsSchema.ts
+++ src/schemas/TagsSchema.ts
@@ -4,3 +4,4 @@ export const addTagsSchema = z.object({
   id: z.string().min(1),
   description: z.string(),
 }).partial({ description: true })
+export type addTagType = z.infer<typeof addTagsSchema>
